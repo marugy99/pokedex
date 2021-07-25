ed278d46266dee98386c5de7b0e73c34d2abf51b
Maria Eugenia Lucena <maru@Marias-MacBook-Pro.local>
2021-07-25 12:55:08 -0400

    Styles to the main page

--- components/Layout.js
+++ components/Layout.js
@@ -2,14 +2,14 @@ import Head from "next/head";
 
 const Layout = ({ title, children }) => {
     return ( 
-        <>
+        <div className='bg-gray-300'>
             <Head>
                 <title>{ title }</title>
             </Head>
-            <main className='container mx-auto min-h-screen pt-4 px-4 bg-gray-300'>
+            <main className='container mx-auto min-h-screen pt-4 px-4'>
                 { children }
             </main>
-        </>
+        </div>
      );
 }
  
--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,58 @@
-export default function Home({ pokemon }) {
-  return (
-    <>
-      <h1 className='text-4xl text-center mt-4'>Pokedex by Maru</h1>
+  export default function Home({ pokemon }) {
+    return (
+      <>
+        <h1 className='text-4xl text-center mt-4'>Pokedex by Maru</h1>
 
-      {pokemon.map(poke => (
-        <div key={poke.name}>
-          <img src={poke.image} alt={poke.name} />
-          <h2>{poke.name.charAt(0).toUpperCase() + poke.name.slice(1)}</h2>
-        </div>
-      ))}
-    </>
-  )
-}
+        {pokemon.map((poke, index) => (
 
+          <div key={poke.name} className='flex justify-center items-center gap-12 bg-gray-100 my-6 rounded-lg w-3/5 mx-auto hover:shadow-lg'>
 
-export async function getStaticProps(context) {
+            <div>
+              <span className='font-bold'>{index + 1 + '. '}</span><h2 className='capitalize inline'>{poke.name}</h2>
+            </div>
 
-  try {
-    const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=150')
+            <img src={poke.image} alt={poke.name} className='w-1/4'/>
 
-    const {results} = await res.json()
-
-    const pokemon = results.map((result, index) => {
-
-    const paddedIndex = ('00' + (index + 1)).slice(-3)
-
-    const image = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${paddedIndex}.png`
-
-      return {
-        ...result,
-        image
-      }
-    })
-
-    return {
-      props: { pokemon } // will be passed to the page component as a prop
-    }
-
-  } catch (err) {
-    console.log(err)
+          </div>
+        ))}
+      </>
+    )
   }
-      
-}
+
+
+  export async function getStaticProps(context) {
+
+    try {
+
+      // Fetch request to the Pokemon API
+      const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=150')
+
+      // Destructure results 
+      const {results} = await res.json()
+
+      // Map through the results, getting each individual result and the index
+      const pokemon = results.map((result, index) => {
+
+        // Make a padded index for the image url, adding two zeroes to the index and then using slice to only get the last three digits. Add one to the index because the start at 0
+        const paddedIndex = ('00' + (index + 1)).slice(-3)
+
+        // Add the padded index to the image url
+        const image = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${paddedIndex}.png`
+
+        // Return an object with all the properties of the result (the name and url) and the image
+        return {
+          ...result,
+          image
+        }
+      })
+      // Return the pokemon constant
+      return {
+        props: { pokemon } // will be passed to the page component as a prop
+      }
+
+    } catch (err) {
+      console.log(err)
+    }
+        
+  }
 
