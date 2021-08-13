2afc43a7daa0a9b77db4627b71421fb6c0bb054e
maru <maru@castos.com>
2021-08-12 20:37:10 -0400

    Improve overall styles

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Link from 'next/link'
 import { useState } from 'react'
-import Image from 'next/image'
   
   export default function Home({ pokemon }) {
 
@@ -10,7 +9,7 @@ import Image from 'next/image'
     const [initialPokemon, setInitialPokemon] = useState(pokemon)
 
     const loadMore = () => {
-      setPokeNum(pokeNum + 11)
+      setPokeNum(pokeNum + initialNum)
     }
 
     const filterPoke = (e) => {
@@ -22,17 +21,18 @@ import Image from 'next/image'
 
     return (
       <>
-        <div className='flex justify-center'>
-          <Image src='/pokebola.svg' alt='pokeball logo' width={30} height={30}/>
-          <h1 className='text-3xl sm:text-4xl text-center text-gray-800 font-bold ml-2'>Pokedex by Maru</h1>
+        <div className='flex justify-center mt-2'>
+          <img src='/pokebola.svg' alt='pokeball logo' className='w-7 h-7 sm:w-10 sm:h-10 mt-1'/>
+          <h1 className='text-3xl sm:text-5xl text-center text-gray-800 font-bold ml-2'>Pokédex</h1>
         </div>
+        <p className='text-center pt-2 text-gray-600 ml-3 sm:text-lg'>Made by <a href='https://github.com/marugy99' target='_blank' className='underline hover:opacity-80'>Maru</a></p>
 
         <input
                 type="text"
                 name="searchBar"
                 id="searchBar"
-                placeholder="Search for a Pokemon"
-                className='border border-gray-200 p-2 block mx-auto sm:w-3/5 w-full my-4 rounded-lg focus:ring focus:outline-none'
+                placeholder="Search for a Pokémon"
+                className='border border-gray-200 p-2 block mx-auto sm:w-3/5 w-full my-4 rounded-lg shadow-md focus:ring focus:outline-none'
                 onKeyUp={filterPoke}
             />
 
@@ -40,29 +40,32 @@ import Image from 'next/image'
           
           {initialPokemon.slice(0, pokeNum).map((poke, index) => (
 
-            <li key={index} className='bg-gray-100 rounded-lg w-full sm:w-auto mx-auto py-6 hover:shadow-lg'>
+            <Link href={`/pokemon/${poke.name}`}>
 
-              <Link href={`/pokemon/${poke.name}`}>
+              <a className='focus:ring focus:outline-none rounded-lg p-0 transform hover:translate-y-1.5 transition-transform duration-500 ease-in-out relative'>
 
-                <a className='flex flex-col items-center focus:ring focus:outline-none'>
-                  <img src={poke.image} alt={poke.name} className='sm:w-6/12 w-3/12'/>
+                <li key={index} className='bg-gray-100 rounded-lg w-full sm:w-auto mx-auto py-6 shadow-md flex flex-col items-center transition hover:bg-green-50'>
+                  
+                  <span className='font-bold text-gray-300 absolute text-5xl md:text-7xl left-4 top-2'>{poke.id}</span>
+                  
+                  <img src={poke.image} alt={poke.name} className='sm:w-6/12 w-3/12 z-10'/>
 
-                  <div><span className='font-bold text-gray-500'>{poke.id + '. '}</span><h2 className='capitalize inline font-bold text-xl sm:text-2xl text-gray-800'>{poke.name}</h2></div>
-                </a>
-              
-              </Link>              
+                  <div><h2 className='capitalize inline font-bold text-xl sm:text-2xl text-gray-800'>{poke.name}</h2></div>
 
-            </li>
+                </li>
+              </a>
+
+            </Link>              
           ))}
 
         </ul>
-        
+
         {/* Show load more btn if: 
-            - the pokemon array is greater than than 0
+            - the pokemon array is greater than 0
             - the pokemon number shown is NOT greater than or equal to the pokemon array
             - the pokemon number shown is NOT greater than or equal to 151 */}
 
-        {initialPokemon.length > 0 && !(pokeNum >= initialPokemon.length) && !(pokeNum >= 151) && <button onClick={loadMore} className="block bg-green-600 py-2 mx-auto rounded-lg w-36 text-white hover:opacity-80 focus:ring focus:outline-none">Load More</button>}
+        {initialPokemon.length > 0 && !(pokeNum >= initialPokemon.length) && !(pokeNum >= 151) && <button onClick={loadMore} className="block bg-green-700 py-2 mx-auto rounded-lg w-36 text-white hover:opacity-80 focus:ring focus:outline-none">Load More</button>}
         {initialPokemon.length <= 0 && <p className='text-center text-gray-800'>No results found!</p>}
         <br />
       </>
